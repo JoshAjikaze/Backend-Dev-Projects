1c2920f6f7e35be0ae32986901b510a6244e36d5
Ajiwojuolorun Joshua <ajiwojuolorunjoshua@gmail.com>
2025-12-08 15:48:40 +0100

    Add database connection setup and update environment variables

---
+++ MoviesReview/src/config/db.js
@@ -0,0 +1,21 @@
+const {PrismaClient} = "@prisma/client"
+
+const prisma = new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
+
+const connectDB = async () => {
+    try {
+        await prisma.$connect();
+        console.log("DB connected via prisma");
+
+    } catch (error) {
+        console.error(`Database conenction error: ${error.message}`);
+        process.exit(1)
+    }
+}
+const disconnectDB = async () => {
+    await prisma.$disconnect();
+}
+
+export { prisma, connectDB, disconnectDB }
--- MoviesReview/src/main.js
+++ MoviesReview/src/main.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB, disconnectDB } from './config/db.js';
 
 // routes imports
 import movieRoutes from './routes/movieRoutes.js'
 
 dotenv.config();
-const PORT = process.env.port || 5000
+// connectDB()
+
+
+const PORT = process.env.PORT || 5001
 
 const app = express()
 app.use(express.json())
@@ -15,3 +19,7 @@ app.use("/movies", movieRoutes)
 const server = app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })
+
+// process.on("unhandledRejection", (err) => {
+//     console.error("Unhandled Rejection:", err);
+// })
